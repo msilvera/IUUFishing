d7c9a43f6bfe445c4a77d9fa98dbe0bc7e7b56c5
msilvera <marisg@adinet.com.uy>
2020-09-28 20:16:03 -0300

    Add processing & parsing of the response files

---
+++ powershel-anp-consulta/Consulta-ANP-OcupacionPuerto.ps1
@@ -0,0 +1,74 @@
+﻿$WebResponseObj = Invoke-WebRequest -Uri "http://aplicaciones.anp.com.uy/montevideo/sistemas/consultas/arribos/ocupacion_puerto.asp" `
+-Method "POST" `
+-Headers @{
+"Cache-Control"="max-age=0"
+  "Origin"="http://aplicaciones.anp.com.uy"
+  "Upgrade-Insecure-Requests"="1"
+  "DNT"="1"
+  "User-Agent"="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36"
+  "Accept"="text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+  "Referer"="http://aplicaciones.anp.com.uy/montevideo/sistemas/consultas/arribos/ocupacion_puerto.asp"
+  "Accept-Encoding"="gzip, deflate"
+  "Accept-Language"="es,en;q=0.9,de;q=0.8,en-US;q=0.7,pt;q=0.6"
+  "Cookie"="ASPSESSIONIDAASARRBS=PFNJIHHDAGOAGEADMINOCIMG; ASPSESSIONIDAABSAQBQ=DNODFBGDMFJOLEDINFNCIPIJ"
+} `
+-ContentType "application/x-www-form-urlencoded" `
+-Body "bn=&co=1&tb=5&ip=1&am=TTTTTTTT&sr=T&Zonas=0&sa=T&Lugares=0&sc=T&o=Z&pfa=T&Submit=++Aceptar++"
+
+$WebResponseObj| Get-Member
+
+
+
+$fecha = Get-Date -format yyyyMMddHHmm
+$folder = 'C:\Users\Mariana\CODE\iuu-fishing-occ\powershel-anp-consulta\resultados' + '\' + $fecha
+
+md $folder
+
+
+$filename =  $folder + '/ANP_ocupacion_puerto_' + $fecha +  '.html'
+
+$WebResponseObj.content >> $filename
+
+
+#Getting table by ID.
+#$oTable = $oHtmlDoc.getElementByID("table6")
+#----------
+
+
+#[regex]$regex = "(?s)<TABLE ID=.*?</TABLE>"
+[regex]$regex ="(?s)<A HREF=.*?>"
+$tables = $regex.matches((GC $filename -raw)).groups.value
+
+$tables
+
+ForEach($string in $tables){
+
+$url = ''
+$uri=''
+$newfile=''
+
+$url = $string -replace '<A HREF="',''
+$url = $url -replace '">',''
+$url
+
+$newfile = (($url -replace '.asp\?','_') -replace '&','_')-replace '=','_'
+$newfile = $folder + '/' + $newfile  + $fecha +  '.html'
+$newfile
+
+$uri = "http://aplicaciones.anp.com.uy/montevideo/sistemas/consultas/arribos/" + $url
+
+$WebResponseObj2 = Invoke-WebRequest -Uri $uri -Headers @{
+"Upgrade-Insecure-Requests"="1"
+  "DNT"="1"
+  "User-Agent"="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36"
+  "Accept"="text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+  "Referer"="http://aplicaciones.anp.com.uy/montevideo/sistemas/consultas/arribos/ocupacion_puerto.asp"
+  "Accept-Encoding"="gzip, deflate"
+  "Accept-Language"="es,en;q=0.9,de;q=0.8,en-US;q=0.7,pt;q=0.6"
+  "Cookie"="ASPSESSIONIDAASARRBS=PFNJIHHDAGOAGEADMINOCIMG; ASPSESSIONIDAABSAQBQ=DNODFBGDMFJOLEDINFNCIPIJ"
+}
+$WebResponseObj2.content >> $newfile
+
+
+}
+
---
+++ powershel-anp-consulta/process-ANP-responses.ps1
@@ -0,0 +1,82 @@
+﻿#List all directories in path
+
+
+$initial_path = 'C:\Users\Mariana\CODE\iuu-fishing-occ\powershel-anp-consulta\resultados2'
+$pathToOutputFile = $initial_path + '\' +  "BrokenFiles.csv"
+$pathToOutputVesselsFile = $initial_path + '\' +  "ListOfVessels.csv"
+
+$folders = Get-ChildItem -Path $initial_path  –Directory
+
+$folders.Count
+
+
+
+For ($i=0; $i -le ($folders.Count-1); $i++) {
+
+    $next_folder = $folders.Item($i) | Select-Object -ExpandProperty  "Name"
+
+    $next_folder = $initial_path + '\'+ $next_folder
+
+    
+    #$init_file = Get-ChildItem -Path $next_folder  –File 'ANP*.html'
+    #$init_file
+    #$list = Get-Content -Path ($next_folder + '\' + $init_file)
+
+    $detail_files = Get-ChildItem -Path $next_folder  –File 'datos*.html'
+    
+    $array = @() #empty array
+    $array2 = @() #empty array
+   [System.Collections.ArrayList] $Broken_files= $array
+   [System.Collections.ArrayList] $ListOfVessels= $array2
+
+   foreach ($detail_file in $detail_files) {
+   
+       $file_content = Get-Content -Path ($next_folder + '\' + $detail_file)
+
+       $data =''
+       $fecha = $detail_file.Name.Substring(62,8)
+
+       foreach($line in $file_content){
+       
+           if ($line.IndexOf('NOMBRE:') -ne -1) 
+           {
+            $data = $line
+
+            break
+
+           }
+       
+       }
+        
+    
+    if ($data -ne '' ) 
+    {
+        
+            $indx= $data.IndexOf('NOMBRE:')
+            $start_indx = $data.IndexOf('<b>',$indx) + 3
+            $end_indx = $data.IndexOf('</b>',$indx)
+            $name = $data.Substring($start_indx,($end_indx -$start_indx))
+           # 'NOMBRE: ' + $name
+
+            $indx= $data.IndexOf('BANDERA:')
+            $start_indx = $data.IndexOf('<b>',$indx) + 3
+            $end_indx = $data.IndexOf('</b>',$indx)
+            $bandera = $data.Substring($start_indx,($end_indx -$start_indx))
+            #'BANDERA: ' + $bandera
+
+            $item = $fecha  + ',' + $name + ',' + $bandera
+            #$item 
+            $ListOfVessels.Add($item.ToString());
+ 
+    }
+   else{
+    #broken file?
+
+    $Broken_files.Add($detail_file);
+   }
+   
+   }
+    $Broken_files | ConvertTo-Csv -NoTypeInformation | Add-Content $pathToOutputFile
+    $ListOfVessels | Add-Content $pathToOutputVesselsFile
+
+}
