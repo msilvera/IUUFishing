f962fac7e41ab4f49629bc5cce9070d8aa4c1202
Mariana Silvera <silvera.mariana@gmail.com>
2021-03-14 22:30:18 -0300

    remove sendgrid key from uploaded code

--- powershel-anp-consulta/SendEmail.ps1
+++ powershel-anp-consulta/SendEmail.ps1
@@ -1,7 +1,7 @@
 ﻿#https://blog.mailtrap.io/azure-send-email/
 
 $user = "apikey"
-$password = "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"
+$password = "*******"
 
 $securePassword = $password | ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString 
 $cred=New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, ($securePassword | ConvertTo-SecureString)
@@ -52,5 +52,5 @@ $SMTPPort = "587"
 #25, 587	(for unencrypted/TLS connections)
 #465	(for SSL connections)
 #Username	apikey
-#Password	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
+#Password	******
 
